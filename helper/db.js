806ccaa699788287cd63e277febeaf39955662a3
Mustafa ULUKAYA <mustafaulukaya@outlook.com.tr>
2019-08-25 17:53:59 +0300

    Mimari değiştirilmesi ve şifremi unuttum akışları

--- helper/db.js
+++ helper/db.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
+const url = require('url');
 
 module.exports = function () {
-    //mongoose.connect('mongodb://admin:y3jG8mW2pMqwFyK@ds231307.mlab.com:31307/heroku_lmx0b541');
-    mongoose.connect('mongodb://localhost/lileduca');
+
+    //mongoose.connect('mongodb://localhost/lileduca');
+
+    mongoose.connect('mongodb://admin:y3jG8mW2pMqwFyK@ds231307.mlab.com:31307/heroku_lmx0b541');
+
     mongoose.connection.on('open',()=> {
         console.log('MongoDB : Connected');
     });
